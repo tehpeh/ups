7e5bb38470dbbeb367ee203e7bb41db27e78cf04
Justin Grubbs <justin@kingandpartners.com>
2018-05-02 10:14:03 -0400

    remove comments

--- lib/ups/parsers/void_shipment_parser.rb
+++ lib/ups/parsers/void_shipment_parser.rb
@@ -4,11 +4,6 @@ module UPS
       attr_accessor :identification_number, :shipment_digest
 
       def value(value)
-        # if switch_active?(:ShipmentIdentificationNumber)
-        #   self.identification_number = value.as_s
-        # elsif switch_active?(:ShipmentDigest)
-        #   self.shipment_digest = value.as_s
-        # end
         super
       end
     end
