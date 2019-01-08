98a676d7530c915016a802688281292d57b068d3
John Royall <johnroyall7@yahoo.com>
2019-01-08 15:26:43 -0500

    quick and dirty minitest spec for addrss builder postal_code method

---
+++ spec/ups/builders/address_builder_minitest_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+class UPS::Builders::TestAddressBuilder < Minitest::Test
+  include SchemaPath
+  include ShippingOptions
+
+  def setup
+    @us_address_builder = UPS::Builders::AddressBuilder.new({country: 'us', state: 'ny', postal_code: '29464'})
+    @ie_address_builder = UPS::Builders::AddressBuilder.new({country: 'ie', state: 'galway', postal_code: ''})
+  end
+
+  def test_allows_postal_code
+    assert_equal(@us_address_builder.postal_code.text, "29464")
+  end
+
+  def test_allows_empty_postal_code
+    assert_equal(@ie_address_builder.postal_code.text, "")
+  end
+end
